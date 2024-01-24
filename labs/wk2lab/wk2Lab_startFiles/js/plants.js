82a54be93c05c10ac3aaa5954c7ad3a6c1022ee7
hahnaseaman <146898561+hahnaseaman@users.noreply.github.com>
2024-01-24 17:27:26 -0500

    Add files via upload

---
+++ labs/wk2lab/wk2Lab_startFiles/js/plants.js
@@ -0,0 +1,7 @@
+// name: Hahna Seaman, date: 1/24/24 
+
+// define variables containing img src values
+
+let blanket = "images/blanket.jpg";
+let bluestem = "images/bluestem.jpg";
+let rugosa = "images/rugosa.jpg";
