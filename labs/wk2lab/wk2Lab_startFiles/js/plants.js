4fbf19964f45dd91520bf07e79ce74a3dbeb02b2
hahnaseaman <146898561+hahnaseaman@users.noreply.github.com>
2024-02-05 17:42:51 -0500

    Delete labs directory

--- labs/wk2lab/wk2Lab_startFiles/js/plants.js
+++
@@ -1,7 +0,0 @@
-// name: Hahna Seaman, date: 1/24/24 
-
-// define variables containing img src values
-
-let blanket = "images/blanket.jpg";
-let bluestem = "images/bluestem.jpg";
-let rugosa = "images/rugosa.jpg";
